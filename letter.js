b72b08fe3adeeb009a578b664ff27ddb836eed79
Nigel Finley <nfinleymusic@gmail.com>
2016-10-09 18:34:55 -0500

    created all necessary js files for the hangman game

---
+++ letter.js
@@ -0,0 +1,3 @@
+// letter.js is a constructor file
+// letter.js should control whether or not a letter appears 
+// as a "_" or as itself on-screen
---
+++ word.js
@@ -0,0 +1,3 @@
+// This will be the constructor file
+// word.js should contain all of the methods which will check 
+// ...the letters guessed versus the random word selected.
